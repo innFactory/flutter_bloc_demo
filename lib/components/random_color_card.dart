e97540a091945ec40ab6a3e929eb4e49c539c3d2
spoeck <toni.spoeck@web.de>
2019-09-22 16:24:54 +0200

    add streambuilder and make card stateless

--- lib/components/random_color_card.dart
+++ lib/components/random_color_card.dart
@@ -1,68 +1,37 @@
-import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc_demo/bloc/bloc_provider.dart';
 import 'package:flutter_bloc_demo/bloc/current_color_bloc.dart';
 
-class RandomColorCard extends StatefulWidget {
-  RandomColorCard({Key key}) : super(key: key);
-
-  _RandomColorCardState createState() => _RandomColorCardState();
-}
-
-class _RandomColorCardState extends State<RandomColorCard> {
-  StreamSubscription<Color> _currentColorListener;
-  Color _currentColor;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // listen to offers from bloc
-    this.initListeners();
-  }
-
-  void initListeners() {
-    this._currentColorListener = BlocProvider.of<CurrentColorBloc>(context)
-        .colorObservable
-        .listen(this._updateColor);
-  }
-
-  @override
-  void dispose() {
-    if (_currentColorListener != null) {
-      _currentColorListener.cancel();
-    }
-    super.dispose();
-  }
-
-  void _updateColor(Color c) {
-    this.setState(() {
-      this._currentColor = c;
-    });
-  }
+class RandomColorCard extends StatelessWidget {
+  const RandomColorCard({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    CurrentColorBloc currentColorBloc =
+        BlocProvider.of<CurrentColorBloc>(context);
+
     return GestureDetector(
-        onTap: _onTap,
+        onTap: () => _onTap(context),
         child: Container(
           height: 200,
           width: 200,
-          child: Card(
-              color: _currentColor,
-              child: Center(
-                child: Text(
-                  "Tap here to generate a new color",
-                  style: TextStyle(color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
-              )),
+          child: StreamBuilder(
+              initialData: Colors.red,
+              stream: currentColorBloc.colorObservable,
+              builder: (BuildContext context, snapShot) => Card(
+                  color: snapShot.data,
+                  child: Center(
+                    child: Text(
+                      "Tap here to generate a new color",
+                      style: TextStyle(color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ))),
         ));
   }
 
-  void _onTap() {
+  void _onTap(BuildContext context) {
     BlocProvider.of<CurrentColorBloc>(context).generateColor();
   }
 }
